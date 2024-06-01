f68353f41f469d7d672f61f23772db2a1822d3ec
Deivinson Pereira <lordpoxtaa@gmail.com>
2024-06-01 15:30:32 -0300

    Adjustments

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-server_1.server.listen(process.env.PORT || 3000, () => console.log(`Porta: ${process.env.PORT || 3000}`));
